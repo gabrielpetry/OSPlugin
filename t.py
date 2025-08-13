f2ae48294cf8bb33ca76820dd94307bbb281856f
Gabriel Petry <24570030+gabrielpetry@users.noreply.github.com>
2025-08-13 16:53:34 -0300

    remove t

--- t.py
+++
@@ -1,5 +0,0 @@
-import subprocess, os
-args = ["~/test.sh"]
-result = subprocess.Popen(args, shell=True, start_new_session=True, text=True, stdout=subprocess.PIPE, cwd=os.path.expanduser("~"))  # If cwd is not set in the flatpak /app/bin/StreamController cannot be found
-
-print(result.communicate()[0].rstrip())
